ad708ad330f4a96ad1d9d441a1213576ea97c469
Nicholas Bracher <youremail@example.com>
2025-09-19 09:23:33 +1000

    Add program with functions to calculate grade based on user input and provide a random score/grade example

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,28 @@
 """
 CP1404/CP5632 - Practical - Nicholas Bracher
-Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Pass")
-else:
-    print("Excellent")
+
+def main():
+    user_score = float(input("Enter score: "))
+    user_grade = calculate_grade(user_score)
+    print(f"User grade: {user_grade}")
+
+    random_score = random.randint(0, 100)
+    random_grade = calculate_grade(random_score)
+    print(f"Random score: {random_score} \nGrade: {random_grade}")
+
+
+def calculate_grade(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Pass"
+    else:
+        return "Excellent"
+
+
+main()
