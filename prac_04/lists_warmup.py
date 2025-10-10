82d507a5a3b37057c8bddb9448b92a2ee2d09f44
Nicholas Bracher <youremail@example.com>
2025-10-10 14:41:09 +1100

    Add basic list one-liners for prac_04

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 - Practical 4 - Nicholas Bracher
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# numbers[0] = 3
+# numbers[-1] = 2
+# numbers[3] = 1
+# numbers[:-1] = Unkown -> Returns everything except the last
+# numbers[3:4] = Unkown -> Returns everything between the 4th element and (Not inclusive) the 5th
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers = False
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, [6, 5, 3]] -> Wrong, it dosen't nest the list
+
+# 1. numbers[0] = "ten"
+# 2. numbers[-1] = 1
+# 3. print numbers[2:]
+# 4. print(9 in numbers)
