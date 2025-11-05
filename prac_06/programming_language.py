8f7b23ba53bb7769fb60d00f065447cb51249af4
Nicholas Bracher <youremail@example.com>
2025-11-05 16:57:54 +1100

    Add pointer arithmetic attribute to ProgrammingLanguage class, add pointer arithmetic data to languages.csv, and update language_file_reader.py to read the new attribute.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,24 +1,47 @@
 """
-CP1404/CP5632 Practical - Nicolas Bracher
-Time estimate: 30 minutes
-Time actual: 42 minutes
+CP1404/CP5632 Practical - Suggested Solution
+Programming Language class with tests.
 """
 
 
 class ProgrammingLanguage:
-    """Represent a Programming Language object."""
+    """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
-        """Initialise a Programming Language instance."""
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
+        """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
+
+    def __repr__(self):
+        """Return string representation of a ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Pointer Arithmetic={self.pointer_arithmetic}"
 
     def is_dynamic(self):
-        """Determine and return True if the programming language is dynamically typed."""
-        return self.typing.lower() == "dynamic"
+        """Determine if language is dynamically typed."""
+        return self.typing == "Dynamic"
+    
+    def is_pointer_arithmetic_supported(self):
+        """Determine if language supports pointer arithmetic."""
+        return self.pointer_arithmetic
 
-    def __str__(self):
-        """Format and return a string of information."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+def run_tests():
+    """Run simple tests/demos on ProgrammingLanguage class."""
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, python, visual_basic]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    run_tests()
