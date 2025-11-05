6f0efdeff4d0d6f4cdda56cc5cb4f2d97d990519
Nicholas Bracher <youremail@example.com>
2025-11-05 19:44:26 +1100

    Add user input to myguitars.py and allow that input to be written out to file and sorted by year

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,18 +4,20 @@ CP1404 Practical 7 - My Guitars
 
 from guitar import Guitar
 
-FILENAME = 'guitars.csv'
+FILENAME = "guitars.csv"
 
 
 def main():
     """Read file of guitar details, save as objects, display."""
-    guitars = process_file(FILENAME)
+    guitars = load_file_contents(FILENAME)
+    new_guitars = get_user_input()
+    guitars += new_guitars   
     guitars.sort()
-    for guitar in guitars:
-        print(guitar)
+    write_to_file(FILENAME, guitars)
+    display_guitars(guitars)
 
 
-def process_file(filename):
+def load_file_contents(filename):
     """Process guitar file and display vintage status."""
     in_file = open(filename, 'r')
     guitars = []
@@ -26,6 +28,35 @@ def process_file(filename):
         guitars.append(guitar)
     in_file.close()
     return guitars
+
+
+def display_guitars(guitars):
+    """Display guitars and their corresponding information."""
+    for guitar in guitars:
+        print(guitar)
+
+
+def get_user_input():
+    "Get user input and return it as a string."
+    guitars = []
+    print("My Guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:,.2f} added.")
+        name = input("Name: ")
+    return guitars
+
+
+def write_to_file(filename, guitars):
+    out_file = open(filename, 'w')
+    lines = [f"{guitar.name},{guitar.year},{guitar.cost}\n" for guitar in guitars]
+    out_file.writelines(lines)
+    out_file.close()
+    return
+
 
 if __name__ == "__main__":
     main()
